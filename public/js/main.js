be4a858ff53a2443ff7a77b9b8c1c6a39d86e7bc
boot1110001 <boot111.0001@gmail.com>
2019-01-11 13:38:18 +0100

    aa

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,30 @@
 // main.js
+
+window.price_usd = 0;
+
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send();
+}
+
+function main() {
+    // this requests the file and executes a callback with the parsed result once
+    //   it is available
+    alert(price_usd);
+    fetchJSONFile('https://api.coinmarketcap.com/v1/ticker/', function(data){
+        // do something with your data
+        // console.log(data);
+        // alert(data[0].id);
+        window.price_usd=data[0].price_usd;
+    });
+    alert(window.price_usd);
+}
